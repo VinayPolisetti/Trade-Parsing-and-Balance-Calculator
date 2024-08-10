66b0d078ca6a8d3d015a3bab844b415915d5c3d4
VinayPolisetti <97217908+VinayPolisetti@users.noreply.github.com>
2024-08-10 12:09:21 +0530

    Implement route and a controller for uploading trade CSV files

---
+++ controllers/tradeController.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+// Handler for uploading and processing trade data from a CSV file
+exports.uploadTradeDataCSV = async (req, res) => {
+  
+};
--- routes/tradeRoutes.js
+++ routes/tradeRoutes.js
@@ -1,5 +1,10 @@
 const express = require("express");
 
+const tradeController = require("../controllers/tradeController");
+
 const router = express.Router();
+
+// Route for uploading trade CSV file in the file feild of form-data
+router.post("/upload", tradeController.uploadTradeDataCSV);
 
 module.exports = router;
